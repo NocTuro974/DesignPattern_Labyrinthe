16025a307775a82f7ef9ec9a5066ef9f810b1202
Amojow <amaury.bloch@devinci.fr>
2017-12-19 10:57:56 +0100

    change

---
+++ DesignPattern_Labyrinthe/Case.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern_Labyrinthe
+{
+    class Case
+    {
+        private ItemPerson itemPerson;
+        private int type;
+        private Items item;
+        private bool fighter;
+
+
+
+        internal Items Item
+        {
+            get
+            {
+                return item;
+            }
+
+            set
+            {
+                item = value;
+            }
+        }
+
+        public bool Fighter
+        {
+            get
+            {
+                return fighter;
+            }
+
+            set
+            {
+                fighter = value;
+            }
+        }
+
+        internal ItemPerson ItemPerson
+        {
+            get
+            {
+                return itemPerson;
+            }
+
+            set
+            {
+                itemPerson = value;
+            }
+        }
+
+        public int Type
+        {
+            get
+            {
+                return type;
+            }
+
+            set
+            {
+                type = value;
+            }
+        }
+    }
+}
---
+++ DesignPattern_Labyrinthe/Items.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern_Labyrinthe
+{
+    class Items : ItemPerson
+    {
+        private int damages;
+
+        public int Damages
+        {
+            get
+            {
+                return damages;
+            }
+
+            set
+            {
+                damages = value;
+            }
+        }
+        public Items(int damages)
+        {
+            this.Damages = damages;
+        }
+    }
+}
